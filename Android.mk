cad6f84a19b43a474bb9727edca9ee60c0188641
William Bellavance <flintman@flintmancomputers.com>
2016-09-06 10:56:53 -0400

    vold:  add in BOARD_REQUIRES_FORCE_VPARTITION
    This will force mount for the virtual parition in the HP touchpad
    Not sure if any other board has this issue but here is a way
    around it
    
    Change-Id: Idc0f9f90f3597ab206566a9ca95913839c35e89b

--- Android.mk
+++ Android.mk
@@ -121,6 +121,10 @@ ifneq ($(TARGET_EXFAT_DRIVER),)
   full_src_files += fs/Exfat.cpp
 endif
 
+ifeq ($(BOARD_REQUIRES_FORCE_VPARTITION),true)
+vold_cflags += -DCONFIG_FORCE_VPARTITION
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
